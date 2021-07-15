d36b5d4dfdfd17c770b2906a6cfea45750da53b8
greylupi <74424178+greylupi@users.noreply.github.com>
2021-07-15 15:52:00 -0400

    Update NmapProject.py
    added json for debugging and fixed key 'version' error

--- NmapProject.py
+++ NmapProject.py
@@ -1,6 +1,7 @@
 import nmap3
 import argparse
 import sys
+import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("Host", help="enter a domain or IP address")
@@ -10,18 +11,38 @@ host = sys.argv[1]
 
 nmap = nmap3.Nmap()
 os_results = nmap.nmap_version_detection(host)
-print(os_results)
+#print(os_results)   #for debugging purposes
 
+jsonoutput = json.dumps(os_results, indent=4)
+print(jsonoutput) #for debugging purposes
 
+print('====================================')
 
-print('============================================================')
+def open_ports():
+    variable = os_results[host]['ports'][i]['portid']
+    variable1 = os_results[host]['ports'][i]['state']
+    variable2 = os_results[host]['ports'][i]['service']['name']
+    variable3 = os_results[host]['ports'][i]['service']['version']
+    print ('[*] Discovered Port: ' + variable)
+    print ('[*] State : ' + variable1)
+    print ('[*] Service : ' + variable2)
+    print ('[*] Version : ' + variable3)
+    print ('====================================')
+
+def filtered_ports():
+    variable = os_results[host]['ports'][i]['portid']
+    variable1 = os_results[host]
+    variable2 = os_results[host]['ports'][i]['service']['name']
+    variable3 = "Filtered"
+    print ('[*] Discovered Port: ' + variable)
+    print ('[*] Service : ' + variable2)
+    print ('[*] State : ' + variable3)
+    print ('====================================')
 
 for i in range(len(os_results[host]['ports'])):
-        variable = os_results[host]['ports'][i]['portid']
-        variable2 = os_results[host]['ports'][i]['service']['name']
-        variable3 = os_results[host]['ports'][i]['service']['version']
-        print ('[*] Discovered Port: ' + variable)
-        print ('[*] Service : ' + variable2)
-        print ('[*] Version : ' + variable3)
-        print ('============================================================')
+    if os_results[host]['ports'][i]['state'] == 'open':
+        open_ports()
+    else:
+        filtered_ports()
+
 
